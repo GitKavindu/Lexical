326760b867eb752f6dcb10b81dd2b2000198cb26
GitKavindu <bakh99810@gmail.com>
2026-07-13 06:39:53 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Editor from './Components/Editor'
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className='editor-wrapper'>
+        <Editor/>
+      </div>
+    </div>   
+  )}
+
+export default App
