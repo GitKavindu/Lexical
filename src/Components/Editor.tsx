31ff2f9a93825d4581fb49b3ba0eb71ad78fb55a
GitKavindu <bakh99810@gmail.com>
2026-07-14 05:36:45 +0530

    added stage 3 contents

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,4 +1,4 @@
-import {$createTextNode, $getRoot, $getSelection, EditorState} from 'lexical';
+import {$createTextNode, $getRoot, $getSelection, $isRangeSelection, EditorState} from 'lexical';
 import {useEffect} from 'react';
 
 import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
@@ -9,13 +9,19 @@ import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
 import {LexicalComposerContext, useLexicalComposerContext} from '@lexical/react/LexicalComposerContext'
 import {$createHeadingNode, HeadingNode} from '@lexical/rich-text'
+import {$setBlocksType} from '@lexical/selection'
+import {ListPlugin} from '@lexical/react/LexicalListPlugin'
+import { ListNode, ListItemNode } from "@lexical/list";
+import { INSERT_ORDERED_LIST_COMMAND,INSERT_UNORDERED_LIST_COMMAND } from "@lexical/list";
 import "./Editor.css"
 
 const theme = {
   // Theme styling goes here
   //...
   heading:{
-    h1:'glyf-editor-h1'
+    h1:'glyf-editor-h1',
+    h2:'glyf-editor-h2',
+    h3:'glyf-editor-h3'
   },
   text: {
     bold: 'glyf-editor-bold',
@@ -36,13 +42,14 @@ function Editor() {
     theme,
     onError,
     nodes : [
-      HeadingNode
+      HeadingNode,ListNode, ListItemNode
     ]
   };
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <MyHeadingPludin/>
+      <ToolBarPlugin/>
+      <ListPlugin/>
       <RichTextPlugin
         contentEditable={
           <ContentEditable 
@@ -58,15 +65,53 @@ function Editor() {
   );
 }
 
+function ToolBarPlugin () :JSX.Element{
+  const [editor]=useLexicalComposerContext();
+  return <div className='toolBarWrapper'>
+    <MyHeadingPludin/>
+    <ListToolBarPlugin/>
+  </div>
+}
+
 function MyHeadingPludin():JSX.Element{
   const [editor]=useLexicalComposerContext();
-  const onclick = (e:React.MouseEvent):void => {
+  const onclick = (tag : 'h1' |  'h2' | 'h3'):void => {
     editor.update(()=>{
-      const root = $getRoot();
-      root.append($createHeadingNode('h1').append($createTextNode('Hello World')));
+      const selection =$getSelection();
+      //this is only range selection. there are other types which is known as
+      //Grid selection and Node Selection
+      if($isRangeSelection(selection)){
+        $setBlocksType(selection,()=> $createHeadingNode(tag))
+      }
     });
   }
-  return <button onClick={onclick}>Heading</button>
+  return <div>
+    {['h1', 'h2', 'h3'].map((tag) => (
+      <button onClick={() => onclick(tag)} key={tag}>
+        {tag.toUpperCase()}
+      </button>
+    ))}
+  </div>
+  
+}
+
+function ListToolBarPlugin():JSX.Element{
+  const [editor]=useLexicalComposerContext();
+  const onclick = (tag : 'ol' |  'ul') :void => {
+    if(tag === 'ol'){
+      editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND,undefined)
+      return
+    }
+    editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND,undefined)
+  }
+  return <div>
+    {['ol','ul'].map((tag) => (
+      <button onClick={() => onclick(tag)} key={tag}>
+        {tag.toUpperCase()}
+      </button>
+    ))}
+  </div>
+  
 }
 
 export default Editor
